5e1bb4acc9d97861d01e19c1ea8e509a14c7e79b
Glain Avila D’Souza <g4glainavila@gmail.com>
2026-03-30 13:51:28 +0530

    Add files via upload

---
+++ app_layout_tsx_0~13zen._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/[root-of-the-server]__0wbwd_e._.css","static/chunks/node_modules__pnpm_0ap46ls._.js","static/chunks/_11rxmj0._.js"],
+    source: "dynamic"
+});
---
+++ app_page_tsx_0mmmt6w._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/0-5p_next_dist_0g0zdo4._.js"],
+    source: "dynamic"
+});
